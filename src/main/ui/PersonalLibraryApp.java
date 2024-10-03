88e5632cddf7e92704949f952a3f8961d23da3aa
Junseo <junseo_park@naver.com>
2024-10-03 13:29:11 -0700

    Added runLibrary method to handle main menu options and user input

--- src/main/ui/PersonalLibraryApp.java
+++ src/main/ui/PersonalLibraryApp.java
@@ -1,11 +1,14 @@
 package ui;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import model.Book;
 
 public class PersonalLibraryApp {
 
     private UserManager userManager;  // Delegate user management to UserManager
     private Scanner input;
+    private ArrayList<Book> books;
 
     // EFFECTS: runs the application
     public PersonalLibraryApp() {
@@ -27,21 +30,21 @@ public class PersonalLibraryApp {
 
         while (running) {
             System.out.println("\nWelcome to Personal Library Management System!");
-            System.out.println("If you are a new user, press 'n'.");
-            System.out.println("If you are an existing user, press 'u'.");
+            System.out.println("If you are a new user, press '1'.");
+            System.out.println("If you are an existing user, press '2'.");
             System.out.println("To exit, press 'e'.");
 
             System.out.print("Please enter your choice: ");
             String command = input.next();  // Using next() for input
 
-            if (command.equals("u")) {
+            if (command.equals("2")) {
                 if (signingIn()) {
                     System.out.println("Login successful! Proceeding to the next step...");
                     running = false; // Exit the loop after successful signingIn
                 } else {
                     System.out.println(" Please try again. Login failed.");
                 }
-            } else if (command.equals("n")) {
+            } else if (command.equals("1")) {
                 registeration();
             } else if (command.equals("e")) {
                 System.out.println("Exiting the application. Goodbye!");
@@ -82,5 +85,38 @@ public class PersonalLibraryApp {
         // Delegate the signingIn process to UserManager
         return userManager.login(username, password);
     }
-    
+     // MODIFIES: this
+    // EFFECTS: processes user input for signingIn or registration
+    private void runLibrary() {
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\n");
+            System.out.println("If you want to add books to library, type '1'.");
+            System.out.println("If you want to add books to wishlist, type '2'.");
+            System.out.println("If you want to see your wishlist, type '3'.");
+            System.out.println("If you want to search for books, type '4'.");
+            System.out.println("To exit, type 'e'.");
+
+            System.out.print("Please enter your choice: ");
+            String command = input.next();  // Using next() for input
+
+            if (command.equals("1")) {
+               //todo
+            } else if (command.equals("2")) {
+               //todo
+            } else if (command.equals("3")) {
+                //todo
+            } else if (command.equals("4")) {
+                //todo
+            } else if (command.equals("e")) {
+                System.out.println("Exiting the application. Goodbye!");
+                running = false; // Exit the loop to end the application
+            } else {
+                System.out.println("Invalid input. Please try again.");
+                runLibrary();
+                return;
+            }
+        }
+    }
 }
