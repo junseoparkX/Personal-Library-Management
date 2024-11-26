7b3ecd8b40f4d908489f01ee60137cb99c5c54d6
Junseo <junseo_park@naver.com>
2024-11-26 12:50:43 -0800

    Added specification for EventLog

---
+++ src/main/model/EventLog.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Represents a log that tracks all recorded events.
+ * Implements the Singleton Design Pattern to ensure a single global instance.
+ */
+public class EventLog implements Iterable<Event> {
+    /** Singleton instance of the EventLog */
+    private static EventLog theLog;
+    private Collection<Event> events;
+
+    /**
+     * Private constructor to enforce Singleton Design Pattern.
+     */
+    private EventLog() {
+
+    }
+
+    /**
+     * Returns the single instance of EventLog, creating it if necessary.
+     * @return the singleton instance of EventLog
+     */
+    public static EventLog getInstance() {
+
+    }
+
+    /**
+     * Records an event in the event log.
+     * @param e the event to be logged
+     */
+    public void logEvent(Event e) {
+
+    }
+
+    /**
+     * Clears the event log and adds a "log cleared" event.
+     */
+    public void clear() {
+
+    }
+
+    /**
+     * Returns an iterator to traverse the logged events.
+     * @return an iterator for the event collection
+     */
+    @Override
+    public Iterator<Event> iterator() {
+        return events.iterator();
+    }
+}
